d2a98206519121639d3321e3c9450de77f043162
Chris Stambula <stamp85@gmail.com>
2018-04-26 21:58:28 +0100

    few ui customizations

--- settings.el
+++ settings.el
@@ -15,12 +15,54 @@
 
 (menu-bar-mode -1)
 
-(set-default-font "Fira Mono 11")
+(setq inhibit-splash-screen t)
+
+(setq frame-title-format
+  '("Emacs - " (buffer-file-name "%f"
+    (dired-directory dired-directory "%b"))))
+
+(set-frame-font "Fira Mono-10")
 
 (use-package doneburn-theme
   :ensure t
   :config (load-theme 'doneburn 'no-confirm))
 
+(line-number-mode 1)
+(column-number-mode 1)
+
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+(use-package ivy
+  :ensure t
+  :config (ivy-mode 1))
+(use-package counsel
+  :ensure t)
+(use-package swiper
+  :ensure t)
+
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-fuzzy)))
+
+(use-package company
+  :ensure t)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(use-package magit
+  :ensure t)
