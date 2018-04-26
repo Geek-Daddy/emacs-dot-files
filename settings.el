f17342d1f244c93a07e6a1661f0661e8fff3dbe5
Chris Stambula <stamp85@gmail.com>
2018-04-26 20:09:55 +0100

    inital config

---
+++ settings.el
@@ -0,0 +1,26 @@
+
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(package-initialize)
+
+(require 'use-package)
+
+(menu-bar-mode -1)
+
+(set-default-font "Fira Mono 11")
+
+(use-package doneburn-theme
+  :ensure t
+  :config (load-theme 'doneburn 'no-confirm))
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
